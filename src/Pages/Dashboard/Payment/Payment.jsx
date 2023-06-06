f8b287e70292227c2b4cd0d372a69a4c2fa93042
mahdee18 <mahdeec10@gmail.com>
2023-06-06 23:14:08 +0600

    Payment form created

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import SectionTitle from '../../../components/SectionTitle';
 import { Helmet } from 'react-helmet-async';
+import PaymentForm from './PaymentForm';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import useCart from '../../../Hooks/useCart';
 
 const Payment = () => {
+    const [cart] = useCart()
+    const total = cart.reduce((sum,item)=> sum+item.price,0);
+    const price = parseFloat(total.toFixed(2))
+    const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK)
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Payment</title>
             </Helmet>
             <SectionTitle subheading='Please Proceed' heading='Payment'></SectionTitle>
+            <Elements stripe={stripePromise}>
+
+            <PaymentForm price={price} ></PaymentForm>
+            </Elements>
         </div>
     );
 };
--- src/Pages/Dashboard/Payment/PaymentForm.jsx
+++ src/Pages/Dashboard/Payment/PaymentForm.jsx
@@ -1,17 +1,32 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import React from 'react';
+import { useState } from 'react';
 
-const PaymentForm = () => {
+const PaymentForm = ({price}) => {
     const stripe = useStripe()
     const elements = useElements()
-    const handleSubmit = event => {
+    const [cardError,setCardError] = useState('')
+    const handleSubmit = async(event) => {
         event.preventDefault()
-        if (!stripe || elements) {
+        if (!stripe || !elements) {
             return
         }
         const card = elements.getElement(CardElement)
         if(card === null){
             return
+        }
+        console.log('Card',card)
+        const {error,paymentMethod}= await stripe.createPaymentMethod({
+            type:'card',
+            card
+        })
+        if(error){
+            setCardError(error.message)
+            console.error('Error', error)
+        }
+        else{
+            setCardError('')
+            console.log('Payment Method', paymentMethod)
         }
     }
 
@@ -34,10 +49,13 @@ const PaymentForm = () => {
                         },
                     }}
                 />
-                <button type="submit" disabled={!stripe}>
+                <button className='btn btn-outline btn-sm' type="submit" disabled={!stripe}>
                     Pay
                 </button>
             </form>
+            {
+                cardError && <p className='text-red-700 ml-10'>{cardError}</p>
+            }
         </div>
     );
 };
